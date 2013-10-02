d9ef584a4f613e0c0227d335ab44983b8504be1e
Zeoffrey <enhancerbd@gmail.com>
2013-10-02 17:30:58 +0200

    compétences design version1 finit

--- competences.php
+++ competences.php
@@ -8,15 +8,83 @@
  <h1 class="titres"> Ce que je maîtrise ! </h1> 
 
 
-<span class="label label-success">Success</span>
-
- <div class="progress">
- 
-  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 90%;">
-    <span class="sr-only">40%</span>
+<div>
+<span class="label label-success">PHP</span>
+  <div class="marg_progress"> 
+    <div class="progress">
+      <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 10%;">
+        <span class="sr-only">10%</span>
+      </div>
+    </div>
   </div>
 </div>
 
+<div>
+<span class="label label-success">Mysql</span>
+  <div class="marg_progress">
+    <div class="progress">
+      <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 0;">
+        <span class="sr-only">0%</span>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div>
+<span class="label label-success">HTML/CSS</span>
+  <div class="marg_progress">
+    <div class="progress">
+      <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 80%;">
+        <span class="sr-only">80%</span>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div>
+<span class="label label-success">Jquery</span>
+  <div class="marg_progress">
+    <div class="progress">
+      <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">
+        <span class="sr-only">40%</span>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div>
+<span class="label label-success">Java</span>
+  <div class="marg_progress">
+    <div class="progress">
+      <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 50%;">
+        <span class="sr-only">50%</span>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div>
+<span class="label label-success">C</span>
+  <div class="marg_progress">
+    <div class="progress">
+      <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+        <span class="sr-only">60%</span>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div>
+<span class="label label-success">C#</span>
+  <div class="marg_progress">
+    <div class="progress">
+      <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 55%;">
+        <span class="sr-only">55%</span>
+      </div>
+    </div>
+  </div>
+</div>
 
  </section>
  
