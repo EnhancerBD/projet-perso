d54a6eeb2845bc9ae7295c4d56611e559f436e84
Zeoffrey <enhancerbd@gmail.com>
2013-10-02 16:58:23 +0200

    changement design compétences

--- competences.php
+++ competences.php
@@ -7,15 +7,9 @@
 <section id="competences" class="content">
  <h1 class="titres"> Ce que je maîtrise ! </h1> 
 
- <div class="div_cmpt" id="label_cmpt">
-<span class="label label-success">Success</span>
-<span class="label label-success">Success</span>
-<span class="label label-success">Success</span>
-<span class="label label-success">Success</span>
-<span class="label label-success">Success</span>
- </div>
 
-<div class="div_cmpt" id="progress_cmpt" >
+<span class="label label-success">Success</span>
+
  <div class="progress">
  
   <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 90%;">
@@ -23,34 +17,6 @@
   </div>
 </div>
 
-<div class="progress">
-
-  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 90%;">
-    <span class="sr-only">40%</span>
-  </div>
-</div>
-
-<div class="progress">
- 
-  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 90%;">
-    <span class="sr-only">40%</span>
-  </div>
-</div>
-
-<div class="progress">
-   
-  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 90%;">
-    <span class="sr-only">40%</span>
-  </div>
-</div>
-
-<div class="progress">
-   
-  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 90%;">
-    <span class="sr-only">40%</span>
-  </div>
-</div>
-</div>
 
  </section>
  
